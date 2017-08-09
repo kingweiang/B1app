4fcc5d6fde8d91d88e3c3b606f21a4dc15002a22
kingweiang <kingweiang@163.com>
2017-08-09 16:50:49 +0800

    navigator跳转，支持android返回键

--- app/entry.js
+++ app/entry.js
@@ -7,10 +7,12 @@
 import React, { Component } from 'react';
 import {
     View,
+    BackHandler
 } from 'react-native'
 
 import {Example1} from 'domain/page/Example1'
 import {Example2} from 'domain/page/Example2'
+import {Example3} from 'domain/page/Example3'
 import {Navigator} from 'react-native-deprecated-custom-components'
 
 export class Entry extends Component {
@@ -18,6 +20,15 @@ export class Entry extends Component {
     constructor(){
         super()
     }
+    // 组件加载完成后，对Android返回键进行监听
+    componentDidMount(){
+        BackHandler.addEventListener('hardwareBackPress', (()=>{
+            const navigator = this.refs.navigator
+            navigator.pop()
+                return true;
+        }).bind(this));
+    }
+
     _renderScene(route, navigator){
 
         console.log(route)
@@ -28,15 +39,18 @@ export class Entry extends Component {
                 return <Example1 navigator={navigator} />
             case "Example2" :
                 return <Example2 navigator={navigator} />
+            case "Example3" :
+                return <Example3 navigator={navigator} />
         }
 
     }
     render() {
         return <Navigator
+            ref="navigator"
             // 最初的路线（第一个页面）
-                initialRoute={{name:"Example1"}}
-                // 渲染场景
-                renderScene={this._renderScene}
+            initialRoute={{name:"Example1"}}
+            // 渲染场景
+            renderScene={this._renderScene}
             />
     }
 }
